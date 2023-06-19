39f179dba4d5d19d1a81d55841cc83ad012b1c6d
martaalcazarr <martaalcazarredondo@gmail.com>
2023-06-19 17:45:45 -0400

    add footer

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,10 +1,9 @@
-import { Link, useLocation } from "@remix-run/react"
+import { Link} from "@remix-run/react"
 import logo from '../../public/img/logo.svg'
+import Navbar from "./navbar"
 
 const Header = () => {
 
-    const location = useLocation()
-    console.log(location)
 
 return (
     <header className="header">
@@ -12,20 +11,8 @@ return (
             <Link to='/' className="logo">
                 <img className="logo" src={logo} alt="img logo" />
             </Link>
-            <nav className="navbar">
-                <Link to="/" className={location.pathname === '/' ? 'active' : ''} >
-                    Index
-                </Link >
-                <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>
-                    About us
-                </Link>
-                <Link to="/store" className={location.pathname === '/store' ? 'active' : ''}>
-                    Store
-                </Link>
-                <Link to="/blog" className={location.pathname === '/blog' ? 'active' : ''}>
-                    Blog
-                </Link>
-            </nav>
+
+            <Navbar/>
         </div>
     </header>
   )
--- app/root.jsx
+++ app/root.jsx
@@ -7,6 +7,7 @@ import {
 } from '@remix-run/react'
 import styles from './styles/index.css'
 import Header from './components/header';
+import Footer from './components/footer';
 
 export function meta() {
     return [
@@ -64,6 +65,7 @@ function Document({children}){
             <body>
                 <Header />
                 {children}
+                <Footer/>
                 <Scripts/>
                 <LiveReload/>
             </body>
