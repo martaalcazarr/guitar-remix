ab506db20520d11725b88d6d213c7d98a75913e7
martaalcazarr <martaalcazarredondo@gmail.com>
2023-06-20 17:24:34 -0400

    fix show images on page

--- app/components/guitar.jsx
+++ app/components/guitar.jsx
@@ -2,17 +2,17 @@
 const Guitar = ({guitar}) => {
 
     const {description, imagen, url, price, name} = guitar
-    console.log(imagen.data.attributes)
+    console.log(imagen.data.attributes.formats.medium.url)
     // console.log(imagen.data.attributes.mime['image/svg+xml'].url);
     // const svgImageSrc = `${imagen.data.attributes.url}?mime=image/svg+xml`;
-
-    const svgImageSrc = imagen.url ;
-
+console.log(imagen)
+   
     return (
       <div className="guitar">
-        <object data={svgImageSrc} type="image/svg+xml">
+        {/* <object data={svgImageSrc} type="image/svg+xml">
           <img src={svgImageSrc} alt="img guitarra" />
-        </object>
+        </object> */}
+        <img src={"http://127.0.0.1:1337" + imagen.data.attributes.formats.medium.url} alt="as" />
         <div className="content">
           <h3>{name}</h3>
           <p className="description">{description}</p>
--- app/models/guitars.server.js
+++ app/models/guitars.server.js
@@ -3,3 +3,7 @@ const response = await fetch(`${process.env.API_URL}/guitars?populate=imagen`)
   const result = await response.json()
   return result
 }
+export async function getGuitar(url){
+    const response = await fetch(`${process.env.API_URL}/guitars?filters[url]=${url}&populate=imagen`)
+    return response.json()
+}
