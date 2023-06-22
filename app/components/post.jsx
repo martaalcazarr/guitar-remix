b90184c2b7ca9aa6b561ed3a5c38e469b7e19c8f
martaalcazarr <martaalcazarredondo@gmail.com>
2023-06-22 17:30:26 -0400

    add format and styles to post

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,12 +1,25 @@
+import { Link } from "@remix-run/react";
+import { formatDate } from "~/utils/helpers";
+
 export default function Post({post}){
     const {attributes} = post
     console.log(post)
     return (
-        <div>
-          <h1>{attributes.title}</h1>
-          <p>{attributes.content}</p>
-          <img src={"http://127.0.0.1:1337" + attributes.image.data.attributes.url} alt="" />
+      <article className="post">
+        <img src={"http://127.0.0.1:1337" + attributes.image.data.attributes.url} alt="" />
+        <div className="content">
+          <h3>{attributes.title}</h3>
+          <p className="date">{formatDate(attributes.publishedAt)}
+          </p>
+          <p className="resume">{attributes.content}</p>
+          <Link className="link" to={`/posts/${attributes.url}`}>Read post</Link>
         </div>
+      </article>
+        // <div>
+        //   <h1>{attributes.title}</h1>
+        //   <p>{attributes.content}</p>
+        //   <img src={"http://127.0.0.1:1337" + attributes.image.data.attributes.url} alt="" />
+        // </div>
       );
     }
 //     return(
--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,6 +1,16 @@
 import { useLoaderData } from "@remix-run/react"
 import Post from "~/components/post"
 import { getPosts } from "~/models/post.server"
+import styles from '~/styles/blog.css'
+
+export function links(){
+  return[
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export function meta({data}){
   return[
