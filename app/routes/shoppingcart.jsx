a3847d5871ea845dc3672a70906bf46b6dba8f21
martaalcazarr <martaalcazarredondo@gmail.com>
2023-06-25 21:05:38 -0400

    add styles and functionality to shopping cart

--- app/routes/shoppingcart.jsx
+++ app/routes/shoppingcart.jsx
@@ -1,7 +1,36 @@
+import styles from '~/styles/shoppingcart.css'
 
+export function links(){
+    return[
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+
+export function meta({data}){
+  return[
+  {
+    title: `Shopping Cart - Guitar Studio`,
+    description: `Guitars, guitar store, guitar courses`
+  }
+]
+}
 function ShoppingCart(){
     return(
-        <div>asd</div>
+        <main className="container">
+            <h1 className="heading">Shopping Cart</h1>
+            <div className="content">
+                <div className='cart'>
+                    <h2>Articles</h2>
+                </div>
+            </div>
+            <aside className="resume">
+                <p>Your order</p>
+                <p>Total: CLP</p>
+            </aside>
+        </main>
     )
 }
 
